f8f269f9cc59d81a9b852a9fb53e06159e05aeb3
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-05-03 16:30:04 +0200

    Remove comments

--- SaveSaviours.Test/Entities/Zip.test.cs
+++ SaveSaviours.Test/Entities/Zip.test.cs
@@ -14,16 +14,11 @@ namespace SaveSaviours.UnitTests.Entities {
                 Latitude = 48.137154F,
                 Longitude = 11.576124F,
             };
-            // munichZip.Latitude = 48.137154;
-            //     munichZip.Longitude = 11.576124;
 
             hamburgZip = new Zip {
                 Latitude = 53.551086F,
                 Longitude = 9.993682F,
             };
-            // hamburgZip = new Zip();
-            // hamburgZip.Latitude = 53.551086;
-            // hamburgZip.Longitude = 9.993682;
         }
 
         [Test]
@@ -33,13 +28,5 @@ namespace SaveSaviours.UnitTests.Entities {
 
             Assert.AreEqual((int)Math.Floor(actual), (int)Math.Floor(expected));
         }
-        // [TestCase(-1)]
-        // [TestCase(0)]
-        // [TestCase(1)]
-        // public void IsPrime_ValuesLessThan2_ReturnFalse(int value) {
-        //     var result = _primeService.IsPrime(value);
-
-        //     Assert.IsFalse(result, $"{value} should not be prime");
-        // }
     }
 }
